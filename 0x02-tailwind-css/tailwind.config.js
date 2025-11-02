d85a92ae54a88aa3e394a1d035da89695cf7467d
Yeabsra Andnet <yeabsraandinet19@gmail.com>
2025-11-02 23:20:53 +0300

    Add files via upload

---
+++ 0x02-tailwind-css/tailwind.config.js
@@ -0,0 +1,8 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"], // This line tells Tailwind to scan files inside the 'src' directory
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
